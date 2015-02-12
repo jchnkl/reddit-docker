15f0864286edf60c2fdb2d5825e7daa15b4ba43e
Jochen Keil <jochen.keil@gmail.com>
2015-02-12 22:20:50 +0100

    Ensure that all hosts can connect to db

--- postgres/reddit_init.sh
+++ postgres/reddit_init.sh
@@ -7,3 +7,8 @@ GRANT ALL PRIVILEGES ON DATABASE reddit TO reddit;
 EOSQL
 
 gosu postgres postgres --single -j reddit < /docker-entrypoint-initdb.d/reddit_functions.sql
+
+cat > /var/lib/postgresql/data/pg_hba.conf << EOF
+local   all             all                                     trust
+host    all             all             0.0.0.0/0               md5
+EOF
